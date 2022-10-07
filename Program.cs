41802313bd7f543ec33fe35ad50d21a9cae113ef
Matheus Cristani <matheus.cristani27@gmail.com>
2022-10-06 22:37:18 -0300

    Improves the board print

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿using board;
 using chess;
 
-ChessPosition pos = new ChessPosition('a', 1);
+try
+{
+    Board board = new Board(8, 8);
 
-Console.WriteLine(pos);
-Console.WriteLine(pos.ToPosition());
+    board.PutPiece(new Tower(board, Color.Black), new Position(0, 0));
+    board.PutPiece(new Tower(board, Color.Black), new Position(1, 3));
+    board.PutPiece(new King(board, Color.Black), new Position(0, 2));
+
+    board.PutPiece(new Tower(board, Color.White), new Position(3, 5));
+
+    Screen.PrintBoard(board);
+}
+catch(BoardException exc)
+{
+    Console.WriteLine(exc.Message);
+}
+
+Console.ReadLine();
+
--- Screen.cs
+++ Screen.cs
@@ -6,12 +6,40 @@ class Screen
     {
         for (int i = 0; i < board.Lines; i++)
         {
+            Console.Write(8 - i + " ");
             for(int j = 0; j < board.Columns; j++){
                 Piece piece = board.GetPiece(i, j);
-                Console.Write(piece != null ? piece : '-' + " ");
+                
+                if(piece != null)
+                {
+                    PrintPiece(piece);
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+
+                Console.Write(" ");
             }
 
             Console.WriteLine();
+        }
+
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void PrintPiece(Piece piece)
+    {
+        if(piece.Color == Color.White)
+        {
+            Console.Write(piece);
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(piece);
+            Console.ForegroundColor = aux;
         }
     }
 }
