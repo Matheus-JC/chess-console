64dd7e07251ff8d2caf0afb35c6b83ede9143780
Matheus Cristani <matheus.cristani27@gmail.com>
2022-10-06 21:30:58 -0300

    Starting implement the board

---
+++ board/Color.cs
@@ -0,0 +1,12 @@
+namespace board;
+
+enum Color 
+{
+    Branca,
+    Preta,
+    Amarela,
+    Azul,
+    Vermelha,
+    Verde,
+    Laranja
+}
---
+++ board/Piece.cs
@@ -0,0 +1,17 @@
+namespace board;
+
+class Piece 
+{
+    public Position Position { get; set; }
+    public Color Color { get; protected set; }
+    public int MovesNumber { get; protected set; }
+    public Board Board { get; set; }
+
+    public Piece(Position position, Color color, Board board)
+    {
+        Position = position;
+        Color = color;
+        Board = board;
+        MovesNumber = 0;
+    }
+}
